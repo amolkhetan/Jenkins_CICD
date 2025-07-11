8b02a330f17b887b716999bc24659e0784a440fb
munisekar-py <munisheak@gmail.com>
2025-07-11 11:50:45 +0530

    Testing WebHook

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Jenkins CI/CD!"
+    return "Hello, Jenkins CI/CD!, This is Confirmed WebHooks works fine"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
