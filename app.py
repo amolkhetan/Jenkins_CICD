76e5f7c09b3ce9602f6f063f916a846319638557
Amol Khetan <89386338+amolkhetan@users.noreply.github.com>
2025-07-13 20:26:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello from Flask! Amol is testing Jenkins cicd pipeline'
+    return 'Hello from Flask! Amol is GitHub Actions'
 
 @app.route('/ping')
 def ping():
