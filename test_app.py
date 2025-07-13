2062ad7dce36f66705876deaa093b0fb213123e2
Amol Khetan <89386338+amolkhetan@users.noreply.github.com>
2025-07-13 08:09:51 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,7 +1,18 @@
+import pytest
 from app import app
 
-def test_home():
-    tester = app.test_client()
-    response = tester.get('/')
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get('/')
     assert response.status_code == 200
-    assert b"Hello, Jenkins CI/CD!" in response.data
+    assert b'Hello from Flask!' in response.data
+
+def test_ping(client):
+    response = client.get('/ping')
+    assert response.status_code == 200
+    assert response.data == b'pong'
