e205a505b86d6f10a0a3697cbeb7171dc1377dfb
Amol Khetan <89386338+amolkhetan@users.noreply.github.com>
2025-07-13 12:27:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello from Flask! Amol is testing jenkins cicd pipeline'
+    return 'Hello from Flask! Amol is testing Jenkins cicd pipeline'
 
 @app.route('/ping')
 def ping():
